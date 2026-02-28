55cd771f9bdda828d5699b11c4dc42166db114d7
Leila Moumou <89880143+Leilamoumou@users.noreply.github.com>
2026-02-28 09:38:30 +0000

    secrets for auth for clarity

--- flowfix-mobile/firebaseConfig.js
+++ flowfix-mobile/firebaseConfig.js
@@ -8,15 +8,11 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0uyMpZzJRxkX2kUkzp613Zzw4Xc1TKFQ",
-  authDomain: "flowfix-65fd1.firebaseapp.com",
-  projectId: "flowfix-65fd1",
-  storageBucket: "flowfix-65fd1.firebasestorage.app",
-  messagingSenderId: "878384279716",
-  appId: "1:878384279716:web:041715c6ae2aa120b23631",
-  measurementId: "G-HD6H5ET5X0"
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
